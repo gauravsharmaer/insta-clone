f90e924a129c3e1cb9978438cbb3dc417b7255ef
gauravsharmaer <gauravsharma701998@example.com>
2023-03-08 21:01:52 +0530

    first commit

---
+++ models/Usermodel.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose");
+const {ObjectId}=mongoose.Schema.Types
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    pic:{
+        type:String,
+        default:"https://res.cloudinary.com/dhcnacghe/image/upload/v1678280915/download_def_pic_wsy4mb.jpg"
+    },
+    followers:[{type:ObjectId,ref:"User"}],
+    following:[{type:ObjectId,ref:"User"}]
+})
+module.exports=mongoose.model("User",userSchema)
